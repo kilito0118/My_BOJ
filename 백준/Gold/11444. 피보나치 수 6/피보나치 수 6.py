cffd230b114168e67f950546928d4ead87cc5e8f
kilito0118 <85441841+kilito0118@users.noreply.github.com>
2023-09-18 11:35:56 +0900

    [Gold II] Title: 피보나치 수 6, Time: 40 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Gold/11444. 피보나치 수 6/피보나치 수 6.py
@@ -0,0 +1,28 @@
+import sys
+def input(): return sys.stdin.readline().rstrip()
+def mt_mult(a,b):
+    x = len(a)
+    y = len(b)
+    mt=[[0 for i in range(x)] for i in range(y)]
+    for i in range(x):
+        for j in range(x):
+            for k in range(y):
+                mt[i][j]+=a[i][k]*b[k][j]
+                mt[i][j]%=1000000007
+    return mt
+def square(a,n):
+    if n==1:
+        return a
+    if n%2==0:
+        mt=square(a,n//2)
+        return mt_mult(mt,mt)
+    else:
+        mt=square(a,n-1)
+        return mt_mult(mt,a)
+
+a=int(input())
+mt=[[1,1],[1,0]]
+
+x=square(mt,a)
+print(x[0][1])
+
