398a17c5cce38b46fd6a813969c940420da1c937
kilito0118 <85441841+kilito0118@users.noreply.github.com>
2023-11-20 20:35:13 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 512 ms, Memory: 82360 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.py
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.py
@@ -5,30 +5,30 @@ def input():return sys.stdin.readline().rstrip()
 
 a,b=map(int,input().split())
 
-gra=[{} for i in range(a+1)]
+gra=[[] for i in range(a+1)]
 for i in range(b):
     x,y,z=map(int,input().split())
-    gra[x][y]=z
-    gra[y][x]=z
+    gra[x].append((z,y))
+    gra[y].append((z,x))
 
 s,e=map(int,input().split())
 
 def dy(start,end):
-    zido = [1000*1000 for i in range(a+1)]
+    vist = [0 for i in range(a+1)]
     q=[]
     heapq.heappush(q,(0,start))
     while q:
         z,x = heapq.heappop(q)
-        if zido[x]<z: continue
-        zido[x]=z
-        
+        if vist[x]==1:
+            continue
+        else:
+            vist[x]=1
         if x==end:
             return z
-        for i in gra[x].items():
-            heapq.heappush(q,(z+i[1],i[0]))
+        for i in gra[x]:
+            heapq.heappush(q,(z+i[0],i[1]))
     else:
         return None
-
 try:
     k=min(dy(1,s)+dy(e,a),dy(1,e)+dy(s,a))+dy(s,e)
     print(k)
