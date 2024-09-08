a8875efa775a6c2a785ea5400ff8f699a078f34f
Park junhyeok <85441841+kilito0118@users.noreply.github.com>
2024-09-08 14:25:54 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 360 ms, Memory: 50356 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -1,38 +1,47 @@
 import sys
+def input():return sys.stdin.readline().rstrip()
 import heapq
 
-def input():return sys.stdin.readline().rstrip()
+#배열 만듬
+#우선순위 큐로 관리
+#한 점을 정하고 그 점에서 가장 작은거랑 연결
+#만약 이미 연결된 거랑 연결하려고 하면 넘어감
+#반복
 
-def set_find(k):
-    if visit[k]==k:
+
+def find(k):
+    #print(k)
+    if k==check[k]:
         return k
-    visit[k] = set_find(visit[k])
-    return visit[k]
-def set_union(q,w):
-    q = set_find(q)
-    w = set_find(w)
-    if q>w:
-        visit[q] = w
+    check[k] = find(check[k])
+    return check[k]
+def union(x,y):
+    x = find(x)
+    y = find(y)
+    if x<=y:
+        check[x] = y
     else:
-        visit[w] = q
-        
-def bfs(x):
-    cnt = 0
-    while q:
-        xnt,x,y = heapq.heappop(q)
-        if(set_find(x)==set_find(y)):
-            continue
-        set_union(x,y)
-        cnt+=xnt
-    else:
-        print(cnt)
-        exit()
-
-a,b=map(int,input().split())
-visit = [i for i in range(a+1)]
+        check[y] = x
 q = []
+a,b = map(int,input().split())
+
 for i in range(b):
-    x,y,cnt=map(int,input().split())
-    heapq.heappush(q,(cnt,y,x))
-    heapq.heappush(q,(cnt,x,y))
-bfs(1)
+    k = list(map(int,input().split()))
+    k[2],k[0] = k[0],k[2]
+    k = tuple(k)
+    #print(k)
+    heapq.heappush(q,k)
+cnt = 0
+check = [i for i in range(a+1)]
+while q:
+    value, x, y = heapq.heappop(q)
+    x1 = find(x)
+    y1 = find(y)
+    if x1==y1:
+        continue
+    union(x,y)
+    cnt+=value
+   
+
+print(cnt)
+
