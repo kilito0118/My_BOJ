a57ce04dab206439e441b160acf59a2464ec1a4a
Park junhyeok <85441841+kilito0118@users.noreply.github.com>
2024-11-01 14:36:16 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.py
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.py
@@ -1,5 +1,11 @@
-a=input()
-if a==a[::-1]:
-    print(1)
-else:
-    print(0)
+def pel(st):
+    try:
+        if st[0]==st[-1]:
+            return pel(st[1:-1])
+        else:
+            return 0
+    except:
+        return 1
+
+a = input()
+print(pel(a))
